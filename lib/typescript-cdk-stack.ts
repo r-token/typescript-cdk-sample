f4f1dc23686d0d7a1a2e2cd5f270f028f9a992e7
Ryan Token <ryantoken13@icloud.com>
2021-04-04 17:40:25 -0500

    Added tags to the app and the networking module

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import { Bucket, BucketEncryption } from "@aws-cdk/aws-s3"
 import { Networking } from './networking'
+import { Tags } from '@aws-cdk/core';
 
 export class TypescriptCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -22,9 +23,11 @@ export class TypescriptCdkStack extends cdk.Stack {
       exportName: 'DocumentsBucketName'
     })
 
-    new Networking(this, 'NetworkingConstruct', {
+    const networkingStack = new Networking(this, 'NetworkingConstruct', {
       maxAzs: 2
     })
+
+    Tags.of(networkingStack).add('Module', 'Networking')
 
   }
 }
