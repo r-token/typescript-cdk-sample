db664fd87005da61ac76f88129ee4abd4043cbde
Ryan Token <ryantoken13@icloud.com>
2021-04-04 16:45:30 -0500

    Initial cdk app with an S3 bucket

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,9 +1,25 @@
 import * as cdk from '@aws-cdk/core';
+import { Bucket, BucketEncryption } from "@aws-cdk/aws-s3"
 
 export class TypescriptCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // Create a new, encrypted, S3 bucket for our documents
+    // Arguments of this are:
+    // // the Stack it exists in (this)
+    // // the ID of the resource (the name of the bucket, DocumentsBucket)
+    // // a properties object for different values that can be passed in (encryption, etc)
+    const bucket = new Bucket(this, 'DocumentsBucket', {
+      encryption: BucketEncryption.S3_MANAGED
+    });
+
+    new cdk.CfnOutput(this, 'DocumentsBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'DocumentsBucketName'
+    })
+
   }
 }
