e77df83432fd0f935f852cd060884d04ecf81628
Ryan Token <ryantoken13@gmail.com>
2021-04-07 18:29:12 -0500

    Added IAM permissions and S# signed URLs for our lambda function <> S3 integration

--- api/getDocuments/index.ts
+++ api/getDocuments/index.ts
@@ -1,12 +1,35 @@
 import { APIGatewayProxyEventV2, Context, APIGatewayProxyStructuredResultV2 } from "aws-lambda"
+import S3 from 'aws-sdk/clients/s3'
 
+const s3 = new S3()
 const bucketName = process.env.DOCUMENTS_BUCKET_NAME
 
 export const getDocuments = async(event: APIGatewayProxyEventV2, context: Context): Promise<APIGatewayProxyStructuredResultV2> => {
     console.log(`Bucket name: ${bucketName}`)
     
-    return  {
-        statusCode: 200,
-        body: 'Success' // needs to be a string (use json.stringify if it's a complex object)
+    try {
+        const { Contents: results } = await s3.listObjects({ Bucket: process.env.DOCUMENTS_BUCKET_NAME! }).promise()
+        const documents = await Promise.all(results!.map(async r => generateSignedURL(r)))
+        return {
+            statusCode: 200,
+            body: JSON.stringify(documents)
+        }
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: error.message
+        }
+    }
+}
+
+const generateSignedURL = async (object: S3.Object): Promise<{ filename: string, url: string, }> => {
+    const url = await s3.getSignedUrlPromise('getObject', {
+        Bucket: bucketName,
+        Key: object.Key!,
+        Expires: (60 * 60) // one hour
+    })
+    return {
+        filename: object.Key!,
+        url: url
     }
 }
--- lib/api.ts
+++ lib/api.ts
@@ -3,6 +3,7 @@ import * as lambda from '@aws-cdk/aws-lambda-nodejs'
 import { Runtime } from '@aws-cdk/aws-lambda'
 import * as s3 from '@aws-cdk/aws-s3'
 import * as path from 'path'
+import * as iam from '@aws-cdk/aws-iam'
 
 interface DocumentManagementAPIProps {
     documentBucket: s3.IBucket
@@ -25,5 +26,15 @@ export class DocumentManagementAPI extends cdk.Construct {
                 DOCUMENTS_BUCKET_NAME: props.documentBucket.bucketName
             }
         })
+
+        const bucketPermissions = new iam.PolicyStatement()
+        bucketPermissions.addResources(`${props.documentBucket.bucketArn}/*`)
+        bucketPermissions.addActions('s3:GetObject', 's3:PutObject')
+        getDocumentsFunction.addToRolePolicy(bucketPermissions)
+
+        const bucketContainerPermissions = new iam.PolicyStatement()
+        bucketContainerPermissions.addResources(props.documentBucket.bucketArn)
+        bucketContainerPermissions.addActions('s3:ListBucket')
+        getDocumentsFunction.addToRolePolicy(bucketContainerPermissions)
     }
 }
